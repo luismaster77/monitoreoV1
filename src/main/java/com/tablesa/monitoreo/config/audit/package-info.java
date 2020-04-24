d40ac677785cd8b58fb505cb45f838d4dbc5661a
luismaster77 <luiseduardoingsis@gmail.com>
2020-04-23 20:26:54 -0500

    Commit inicial aplicación

---
+++ src/main/java/com/tablesa/monitoreo/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.tablesa.monitoreo.config.audit;
---
+++ src/main/java/com/tablesa/monitoreo/config/dbmigrations/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * MongoDB database migrations using MongoBee.
+ */
+package com.tablesa.monitoreo.config.dbmigrations;
